f3077469af1bba9ab3e8f90500a4714b73d5feb4
Zak Espley <zespley@gmail.com>
2016-08-29 15:14:30 -0700

    Added Zak's Pixi Test HTML

--- apps/Game/urls.py
+++ apps/Game/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     # url(r'^admin/', admin.site.urls),
-    url(r'^', views.index),
+    url(r'^$', views.index),
+    url(r'zak', views.zak)
 ]
--- apps/Game/views.py
+++ apps/Game/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
 	return render(request, 'Game/index.html')
+
+def zak(request):
+	return render(request, 'Game/Zak_Pixi.html')
